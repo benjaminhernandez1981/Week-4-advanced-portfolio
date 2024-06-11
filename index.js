29b1aee8d59d3414a456caf01f03e32fd2eccc8f
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-06-10 20:50:45 -0500

    wk 4 Advanced Portfolio

---
+++ index.js
@@ -0,0 +1,28 @@
+// template_uvtfft2
+// service_4jmgxz3
+// 982p7mGRS4PRHcotF
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_4jmgxz3",
+      "template_uvtfft2",
+      event.target,
+      "982p7mGRS4PRHcotF"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on benjamineric2005@gmail.com"
+      );
+    });
+}
