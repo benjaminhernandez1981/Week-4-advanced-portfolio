0f367083ae68bb073e7aa014d944953fa0f6ec80
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-06-12 15:45:33 -0500

    WK 4 Advanced Portfolio

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//
-//
-//
+//service_4jmgxz3
+//template_uvtfft2
+//982p7mGRS4PRHcotF
 
 function contact(event) {
   event.preventDefault();
@@ -8,7 +8,7 @@ function contact(event) {
   const success = document.querySelector(".modal__overlay--success");
   loading.classList += " modal__overlay--visible";
   emailjs
-    .sendfor(
+    .sendForm(
       "service_4jmgxz3",
       "template_uvtfft2",
       event.target,
@@ -21,17 +21,7 @@ function contact(event) {
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
       alert(
-        "The email service is temporarily unavailable. Please contact me at benjamineric2005@gmail.com"
+        " The email service is temporarly unavailable. Please contact me directly on benjamineric2005@gmail.com"
       );
     });
-}
-
-let isModalOpen = false;
-function toggleModal() {
-  if (isModalOpenodalOpen) {
-    isModalOpen = false;
-    return document.body.classList.remove("modal--open");
-  }
-  isModalOpen = true;
-  document.body.classList += " modal--open";
 }
